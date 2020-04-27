dc8877b62e843b37ed8a5c2d788d311d6cdf0dc1
Memon Ishan Abdulsattar <53232674+ishanmemon@users.noreply.github.com>
2020-04-27 17:35:53 +0530

    update

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -1,5 +1,10 @@
 
-var speakWord = "Good Bye";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+(function (window){
+  const speakWord = "Good Bye";
+  let byeSpeaker = {};
+
+  byeSpeaker.speak = function(name) {
+    console.log(`${speakWord} ${name}`);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
